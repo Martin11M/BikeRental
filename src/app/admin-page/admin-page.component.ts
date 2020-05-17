51d89e100c2059f8cdcb16450ffcc085d5d24a28
ANadulny <adrian7991@wp.pl>
2020-05-17 18:40:37 +0200

    added dashboard scratch

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../app.component';
 
 @Component({
   selector: 'app-admin-page',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent implements OnInit {
-
+  user:  User;
+  
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      login: `MyLoginName`,
+      isAdmin: true
+    }
   }
 
+  get login() {
+    return this.user.login;
+  }
+
+  get isAdmin() {
+    return this.user.isAdmin;
+  }
 }
