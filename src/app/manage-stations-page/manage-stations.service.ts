23d4dda7ba3da1184c07e8aac0db56bd49d3fd25
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2020-06-21 17:12:08 +0200

    Fixed gray color while covering admin tile buttons.

--- src/app/manage-stations-page/manage-stations.service.ts
+++ src/app/manage-stations-page/manage-stations.service.ts
@@ -9,14 +9,14 @@ export class ManageStationsService {
 
   private addStationForm: FormGroup;
   private initialValues: any;
-  
+
   constructor(private fb: FormBuilder) {
     this.addStationForm = this.fb.group({
       address: ['', [Validators.required, Validators.maxLength(50)]],
       lat: ['52.2309246', [Validators.required, Validators.pattern("^-?[0-9]+(\.[0-9]{1,})?$")]],
       lng: ['21.00893', [Validators.required, Validators.pattern("^-?[0-9]+(\.[0-9]{1,})?$")]],
     });
-    this.initialValues = this.addStationForm.value;;
+    this.initialValues = this.addStationForm.value;
   }
 
   getAddStationForm() : FormGroup {
@@ -24,31 +24,31 @@ export class ManageStationsService {
     return this.addStationForm;
   }
 
-  
+
   getStations(): Station[] {
     // TODO - connect to backend
     // stations data should be pulled from database
 
     let tempUsers: Station[] = [
-      { 
+      {
         stationId: 1,
         address: "Some street 11",
         lat: 52.2352,
         lng: 21.0050,
       },
-      { 
+      {
         stationId: 2,
         address: "Some street 64",
         lat: 52.2360,
         lng: 21.0059,
       },
-      { 
+      {
         "stationId": 3,
         "address": "Other street 22",
         lat: 52.2370,
         lng: 21.0052,
       },
-      { 
+      {
         stationId: 4,
         address: "Name name 10",
         lat: 52.2350,
@@ -61,7 +61,7 @@ export class ManageStationsService {
 
   addStation(address: string, lat: number, lng: number) {
     // TODO - connect to backend
-    // add the station to database 
+    // add the station to database
     console.log(`[TODO] A station should be added to db with address ${address}, latitude ${lat} and longitude ${lng}`);
   }
 }
